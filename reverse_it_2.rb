d9e404d3049d5fc62c8d3410f578c59af1913467
Chris Danielson <chris.a.danielson@gmail.com>
2018-03-20 12:52:28 -0700

    two different approaches and rubocop fixes

--- reverse_it_2.rb
+++ reverse_it_2.rb
@@ -1,15 +1,17 @@
 # REVERSE IT (Part 2)
 # input: string containing one or more words
 # output: string is returned with five letter (or longer) words reversed
-# 
-# string only contains letters and spaces, and spaces only with more than one word
+#
+# string only contains letters and spaces
+# only have spaces when there is more than one word
 #
 # examples/test cases
 # puts reverse_words('Professional')           => lanoisseforP
 # puts reverse_words('Walk around the block')  => Walk dnuor the kcolb
 # puts reverse_words('Launch School')          => hcnuaL loohcS
 #
-# data structures: an array to store split words, string methods to perform reversals
+# data structures
+# an array to store split words, string methods to perform reversals
 #
 # algorithm
 # split string into its component words and save them in an array
@@ -22,14 +24,33 @@
 
 def reverse_words(string)
   words = string.split
-  for word in words
-    if word.length >= 5
-      word.reverse!
-    end
-  end
-  reversed_string = words.join(' ')
+  words.each { |word| word.reverse! if word.length >= 5 }
+  words.join(' ')
 end
 
 puts reverse_words('Professional')
 puts reverse_words('Walk around the block')
 puts reverse_words('Launch School')
+
+def reverse_words_2(string)
+  words = []
+  string.split.each do |word|
+    word.reverse! if word.size >= 5
+    words << word
+  end
+
+  words.join(' ')
+end
+
+puts reverse_words_2('Professional')
+puts reverse_words_2('Walk around the block')
+puts reverse_words_2('Launch School')
+
+def reverse_words_3(string)
+  words = string.split.map { |word| word.length >= 5 ? word.reverse : word }
+  words.join(' ')
+end
+
+puts reverse_words_3('Professional')
+puts reverse_words_3('Walk around the block')
+puts reverse_words_3('Launch School')
